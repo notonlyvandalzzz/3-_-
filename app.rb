382dd193bba17605b1b8efc52599ace698f3ecbd
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-08-22 18:40:04 +1000

    AR init

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barber.db"
+
+class Client < ActiveRecord::Base
+end
 
 configure do
   enable :sessions
