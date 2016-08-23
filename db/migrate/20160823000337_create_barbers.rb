1a9a3a363f5743e79e9c04b9a18482a279bdb6fb
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-08-23 10:10:59 +1000

    B migr mod v3

--- db/migrate/20160823000337_create_barbers.rb
+++ db/migrate/20160823000337_create_barbers.rb
@@ -3,12 +3,12 @@ class CreateBarbers < ActiveRecord::Migration
   	  	create_table :barbers do |t|
   		t.text :name
   		t.timestamps
-
+  		end
   		Barbers.create :name => 'Darth Wader'
   		Barbers.create :name => 'Luke Skywalker'
   		Barbers.create :name => 'R2D2'
   		Barbers.create :name => 'C3PO'
   		Barbers.create :name => 'Yoda'
-  		end
+  		
   end
 end
